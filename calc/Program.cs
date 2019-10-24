2c79d35990dace7fc01e96ad22c8545a46561312
testcalc <https://github.com/sanyo7931/testcalc.git>
2019-10-24 20:25:20 +0800

    測試

---
+++ calc/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calc calc = new calc();
+            int s1 = calc.Add(3, 5);
+            int s2 = calc.Sub(7, 2);
+            Console.WriteLine("3+5=" + s1);
+            Console.WriteLine("7-2=" + s2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ calc/calc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calc
+{
+    public class calc
+    {
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+        public int Sub(int x,int y)
+        {
+            return x - y;
+        }
+    }
+}
